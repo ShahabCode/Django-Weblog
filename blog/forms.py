8695597557a11a07b396ed7a3f8298288e696bde
ShahabCode <shahabepic@gmail.com>
2026-05-20 15:40:00 +0330

    Add TicketForm and CommentForm with validation

---
+++ blog/forms.py
@@ -0,0 +1,36 @@
+from django import forms
+from .models import Comment
+
+class TicketForm(forms.Form):
+    STATUS_CHOICES = (
+        ('پیشنهاد', 'پیشنهاد'),
+        ('انتقاد', 'انتقاد'),
+        ('گزارش', 'گزارش'),
+    )
+    message = forms.CharField(widget=forms.Textarea, required=True)
+    name = forms.CharField(max_length=250, required=True)
+    email = forms.EmailField()
+    phone = forms.CharField(max_length=11, required=True)
+    subject = forms.ChoiceField(choices=STATUS_CHOICES)
+
+    def clean_phone(self):
+        phone = self.cleaned_data['phone']
+        if phone:
+            if not phone.isnumeric():
+                raise forms.ValidationError("شماره تلفن وارد شده عددی نیست!")
+            else:
+                return phone
+
+
+class CommentForm(forms.ModelForm):
+    def clean_name(self):
+        name = self.cleaned_data['name']
+        if name:
+            if len(name) < 3:
+                raise forms.ValidationError("نام وارد شده کوتاه است!")
+            else:
+                return name
+
+    class Meta:
+        model = Comment
+        fields = ('name', 'body')
