881fc0f776a13461a77a693d39520c5f4eaf7df9
ShahabCode <shahabepic@gmail.com>
2026-05-23 18:28:56 +0330

    Add post creation URL route

--- blog/urls.py
+++ blog/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     # path('posts/', views.post_list, name='post_list'),
     path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/add/', views.add_post, name='post_add'),
     path('posts/<pk>/', views.post_detail, name='post_detail'),
     # path('posts/<pk>/', views.PostDetailView.as_view(), name='post_detail'),
     path('ticket/', views.ticket, name='ticket_list'),
