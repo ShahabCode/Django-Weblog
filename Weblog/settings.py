114f6c5f4ba5aa706aa1bf34014b73899fb2c917
ShahabCode <shahabepic@gmail.com>
2026-05-26 19:18:49 +0330

    switch database to PostgreSQL

--- Weblog/settings.py
+++ Weblog/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'Weblog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog_admin',
+        'PASSWORD': 'ShahabCode',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
