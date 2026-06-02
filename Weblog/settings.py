aefac41601743d10aa38a851f380090b03f84348
ShahabCode <shahabepic@gmail.com>
2026-06-02 19:08:26 +0330

    add password reset/change urls and templates

--- Weblog/settings.py
+++ Weblog/settings.py
@@ -147,3 +147,5 @@ DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = False
 LOGIN_REDIRECT_URL = '/blog/profile/' # برای نکست
 LOGIN_URL = '/blog//login/'
 LOGOUT_URL = '/blog//logout/'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
