7d2e56ec833a6b49a6c15190d84bd897ad6de78b
ShahabCode <shahabepic@gmail.com>
2026-06-02 17:54:25 +0330

    add category filtering and login routes

--- blog/urls.py
+++ blog/urls.py
@@ -6,10 +6,11 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     # path('posts/', views.post_list, name='post_list'),
-    path('posts/', views.PostListView.as_view(), name='post_list'),
+    path('posts/', views.post_list, name="post_list"),
+    path('posts/<str:category>', views.post_list, name="post_list_category"),
     path('posts/<pk>/', views.post_detail, name='post_detail'),
     # path('posts/<pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('ticket/', views.ticket, name='ticket_list'),
+    path('ticket', views.ticket, name='ticket'),
     path('posts/<post_id>/comment/', views.post_comment, name='post_comment'),
     path('search/', views.post_search, name='post_search'),
     path('profile/', views.profile, name='profile'),
@@ -17,5 +18,5 @@ urlpatterns = [
     path('profile/create_post/<post_id>', views.edit_post, name='edit_post'),
     path('profile/delete_post/<post_id>', views.delete_post, name='delete_post'),
     path('profile/delete_image/<image_id>', views.delete_image, name='delete_image'),
-
+    path('login', views.user_login, name='login'),
 ]
