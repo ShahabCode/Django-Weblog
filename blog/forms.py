8b42bcc01dd75333cfb32fc685d7549ea791a6df
ShahabCode <shahabepic@gmail.com>
2026-06-08 17:48:16 +0330

    refactor: update forms and improve UI

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,11 @@
+from cProfile import label
+
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-
 from .models import Comment, Post, User, Account
-
+from django.contrib.auth.forms import AuthenticationForm
 
 class TicketForm(forms.Form):
-    STATUS_CHOICES = (
+    SUBJECT_CHOICES = (
         ('پیشنهاد', 'پیشنهاد'),
         ('انتقاد', 'انتقاد'),
         ('گزارش', 'گزارش'),
@@ -14,13 +14,13 @@ class TicketForm(forms.Form):
     name = forms.CharField(max_length=250, required=True)
     email = forms.EmailField()
     phone = forms.CharField(max_length=11, required=True)
-    subject = forms.ChoiceField(choices=STATUS_CHOICES)
+    subject = forms.ChoiceField(choices=SUBJECT_CHOICES)
 
     def clean_phone(self):
         phone = self.cleaned_data['phone']
         if phone:
             if not phone.isnumeric():
-                raise forms.ValidationError("شماره تلفن وارد شده عددی نیست!")
+                raise forms.ValidationError("شماره تلفن عددی نیست!")
             else:
                 return phone
 
@@ -30,55 +30,120 @@ class CommentForm(forms.ModelForm):
         name = self.cleaned_data['name']
         if name:
             if len(name) < 3:
-                raise forms.ValidationError("نام وارد شده کوتاه است!")
+                raise forms.ValidationError("نام کوتاه است")
             else:
                 return name
 
     class Meta:
         model = Comment
-        fields = ('name', 'body')
-
-
-class CreatePostForm(forms.ModelForm):
-    image1 = forms.ImageField(label="تصویر اول", required=False)
-    image2 = forms.ImageField(label="تصویر دوم", required=False)
-
-    class Meta:
-        model = Post
-        fields = ('title', 'description', 'reading_time', 'category')
-
+        fields = ['name', 'body']
+        widgets={
+            'body': forms.TextInput(attrs={
+                'placeholder': 'متن',
+                'class':'form-control'
+            }),
+            'name': forms.TextInput(attrs={
+                'placeholder': 'نام',
+                'class': 'form-control'
+            }),
+        }
 
 class SearchForm(forms.Form):
     query = forms.CharField()
 
+class CreatePostForm(forms.ModelForm):
+    image1 = forms.ImageField(label="تصویر اول", widget=forms.FileInput(attrs={'class': 'form-control'}))
+    image2 = forms.ImageField(label="تصویر دوم", widget=forms.FileInput(attrs={'class': 'form-control'}))
+    class Meta:
+        model = Post
+        fields = ['title', 'description', 'reading_time', 'category']
+        widgets={
+            'title': forms.TextInput(attrs={
+                'class':'form-control',
+                'id' : "con-name",
 
-# class LoginForm(forms.Form):
-#     username = forms.CharField(required=True)
-#     password = forms.CharField(required=True ,widget=forms.PasswordInput)
+            }),
+            'description': forms.Textarea(attrs={
+            	'class': 'form-control',
+				 'rows': '5',
+            }),
+            'reading_time': forms.NumberInput(attrs={
+                'class': 'form-control',
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-select',
+            }),
 
 
-class UserRegistrationForm(forms.ModelForm):
-    password = forms.CharField(max_length=20, widget=forms.PasswordInput, label='password')
-    password2 = forms.CharField(max_length=20, widget=forms.PasswordInput, label='repeat password')
+        }
 
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(max_length=250, required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(max_length=250, required=True, widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UserRegisterForm(forms.ModelForm):
+    password = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='پسورد')
+    password2 = forms.CharField(max_length=20, widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='تکرار پسورد')
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email')
+        fields = ['username', 'first_name', 'last_name' , 'email']
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'form-control',
 
+            }),
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+            }),
+        }
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise forms.ValidationError('پسوردها مطابقت ندارند!')
+            raise forms.ValidationError('پسورد ها مطابقت ندارند!')
         return cd['password2']
-
 
 class UserEditForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email')
+        fields = ['first_name', 'last_name', 'email']
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'class': 'form-control',
 
+            }),
+            'last_name': forms.TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+            }),
+        }
 
 class AccountEditForm(forms.ModelForm):
     class Meta:
         model = Account
-        fields = ('date_of_birth', 'bio', 'job', 'photo')
+        fields = ['date_of_birth', 'bio', 'job', 'photo']
+        widgets = {
+            'date_of_birth': forms.DateInput(attrs={
+                'class': 'form-control',
+            }),
+            'bio': forms.Textarea(attrs={
+                'class': 'form-control',
+                'rows': '5',
+            }),
+            'job': forms.TextInput(attrs={
+                'class': 'form-control',
+
+            }),
+            'photo': forms.FileInput(attrs={
+                'class': 'form-control',
+            }),
+        }
